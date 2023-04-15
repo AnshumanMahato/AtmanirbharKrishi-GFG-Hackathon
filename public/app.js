349eaf53b4b9653b2c659586eb899c3b5fcf93da
vinay kumar <vkumar8192449@gmail.com>
2023-04-15 22:59:20 +0530

    checkout functionality

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+const { doc } = require("prettier");
+
 $(document).ready(function () {
     $("a").on("click", function (event) {
         if (this.hash !== "") {
@@ -20,10 +22,21 @@ $(document).ready(function () {
 $(".menu-items a").click(function () {
     $("#checkbox").prop("checked", false);
 });
-function login_clicked(e){
-    e.style.display="none";
-    let tp1=document.getElementById('after_login1');
-    let tp2=document.getElementById('after_login2');
-    tp1.style.display="block";
-    tp2.style.display="block";
+function login_clicked(e) {
+    e.style.display = "none";
+    let tp1 = document.getElementById('after_login1');
+    let tp2 = document.getElementById('after_login2');
+    tp1.style.display = "block";
+    tp2.style.display = "block";
+}
+function price(x) {
+    document.getElementById('quan').value = 1;
+    document.getElementById('amount').innerText = x;
+    let tp = document.getElementById('quan').value;
+    document.getElementById('net_a').innerText = ` ${x * tp} /-`;
+}
+function net_price() {
+    let x = document.getElementById('amount').innerText;
+    let tp = document.getElementById('quan').value;
+    document.getElementById('net_a').innerText = ` ${x * tp} /-`;
 }
