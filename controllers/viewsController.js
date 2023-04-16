d028807a67d0c0d5d5f5b50ccb00435196116603
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-04-16 09:43:54 +0530

    final done

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,5 @@
 const User = require('../models/userModel');
 const Product = require('../models/productModel');
-// const userController = require('./userController');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -30,15 +29,9 @@ exports.getSingupForm = (req, res) => {
 };
 
 exports.getLoginForm = (req, res) => {
-  res
-    .status(200)
-    // .set(
-    //   'Content-Security-Policy',
-    //   "connect-src 'self' http://127.0.0.1:3000/ https://cdnjs.cloudflare.com"
-    // )
-    .render('login', {
-      title: 'Log into your account'
-    });
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
 };
 
 exports.getAccount = (req, res) => {
@@ -63,22 +56,5 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
   res.status(200).render('account', {
     title: 'Your account',
     user: updatedUser
-  });
-});
-
-exports.edit = catchAsync(async (req, res, next) => {
-  const userdata = await User.findOne({ _id: req.params.id }).select('+active');
-
-  if (!userdata)
-    next(
-      new AppError(
-        'User not found. It might have been deleted. Please refresh to get the current list of users.',
-        404
-      )
-    );
-
-  res.status(200).render('editUser', {
-    title: 'Edit User',
-    userdata
   });
 });
